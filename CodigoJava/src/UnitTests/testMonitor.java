1b228f45927684b2a25c9edc821ebc17a2f21738
CondorNegro <lopez.gaston.1996@gmail.com>
2018-02-02 16:22:35 -0300

    Test iniciado de monitor

---
+++ CodigoJava/src/UnitTests/testMonitor.java
@@ -0,0 +1,101 @@
+/**
+ * 
+ */
+package UnitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class testMonitor {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link Monitor.Monitor#getInstance()}.
+	 */
+	@Test
+	public void testGetInstance() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Monitor.Monitor#configRdp(java.lang.String)}.
+	 */
+	@Test
+	public void testConfigRdp() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Monitor.Monitor#setPolitica(int)}.
+	 */
+	@Test
+	public void testSetPolitica() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Monitor.Monitor#quienesEstanEnColas()}.
+	 */
+	@Test
+	public void testQuienesEstanEnColas() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Monitor.Monitor#andVector(java.util.List, java.util.List)}.
+	 */
+	@Test
+	public void testAndVector() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Monitor.Monitor#isNotAllZeros(java.util.List)}.
+	 */
+	@Test
+	public void testIsNotAllZeros() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Monitor.Monitor#dispararTransicion(int)}.
+	 */
+	@Test
+	public void testDispararTransicion() {
+		fail("Not yet implemented");
+	}
+
+}
