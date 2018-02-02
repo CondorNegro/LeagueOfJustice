038f3b64da675b6b4b18e8b2e2824f92dc4e268c
CondorNegro <lopez.gaston.1996@gmail.com>
2018-02-02 00:45:03 -0300

    Unit test politica iniciado

---
+++ CodigoJava/src/UnitTests/testPolitica.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package UnitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Gastón
+ *
+ */
+public class testPolitica {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link Monitor.Politica#Politica(int)}.
+	 */
+	@Test
+	public void testPolitica() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Monitor.Politica#cualDisparar(java.util.List)}.
+	 */
+	@Test
+	public void testCualDisparar() {
+		fail("Not yet implemented");
+	}
+
+}
