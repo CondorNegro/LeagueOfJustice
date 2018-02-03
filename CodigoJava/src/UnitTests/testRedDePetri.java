035d8b6686f5a270d4d64254932eb0e5ec30956a
CondorNegro <lopez.gaston.1996@gmail.com>
2018-02-03 16:17:13 -0300

    RedDePetri class hecha. Falta test

---
+++ CodigoJava/src/UnitTests/testRedDePetri.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ */
+package UnitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class testRedDePetri {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link Monitor.RedDePetri#RedDePetri(java.lang.String)}.
+	 */
+	@Test
+	public void testRedDePetri() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Monitor.RedDePetri#getCantTransiciones()}.
+	 */
+	@Test
+	public void testGetCantTransiciones() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Monitor.RedDePetri#getSensibilizadas()}.
+	 */
+	@Test
+	public void testGetSensibilizadas() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Monitor.RedDePetri#disparar(int)}.
+	 */
+	@Test
+	public void testDisparar() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Monitor.RedDePetri#getMarcadoSiguiente(int)}.
+	 */
+	@Test
+	public void testGetMarcadoSiguiente() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Monitor.RedDePetri#esDisparoValido(int[][])}.
+	 */
+	@Test
+	public void testEsDisparoValido() {
+		fail("Not yet implemented");
+	}
+
+}
