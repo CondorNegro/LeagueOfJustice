5886113fd0f14c3ef126d2830b8a96c8a56eae8e
CondorNegro <lopez.gaston.1996@gmail.com>
2018-02-06 12:10:24 -0300

    Cola comentado

--- CodigoJava/src/Monitor/Cola.java
+++ CodigoJava/src/Monitor/Cola.java
@@ -1,5 +1,6 @@
 package Monitor;
 
+//Existe una cola por transicion
 
 public class Cola { 
 	private int cantHilosEnCola;
@@ -13,17 +14,17 @@ public class Cola {
 	}
 	
 	public synchronized void resume(){
-		notify();
+		notify(); //El hilo sale de la cola
 	}
 	
 	
 	public synchronized void delay() throws InterruptedException{
 		this.cantHilosEnCola++;
-		super.wait();
-		this.cantHilosEnCola--;
+		super.wait(); //El hilo entra a la cola
+		this.cantHilosEnCola--; //Cuando sale, resta la cantidad de hilos
 	}
 	
-	public boolean isEmpty(){
+	public boolean isEmpty(){ //Devuelve true si no hay hilos bloqueados en la cola
 		if(cantHilosEnCola==0){
 			return true;
 		}
