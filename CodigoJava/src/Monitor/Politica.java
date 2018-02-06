aefa5b16d74f33d46d70403c5c069843ec49eb05
CondorNegro <lopez.gaston.1996@gmail.com>
2018-02-06 12:07:57 -0300

    Politica comentado

--- CodigoJava/src/Monitor/Politica.java
+++ CodigoJava/src/Monitor/Politica.java
@@ -3,9 +3,15 @@ package Monitor;
 import java.util.List;
 
 public class Politica { 
+	//Vector indicando la prioridad de las transiciones.
+	//{2,5,4} -> indica que la transición 2 es la de mayor prioridad. La t4 es la de menor prioridad.
 	private static final int[] transicionesPrioritariasSubida={1,2,3,4}; //Corregir
 	private static final int[] transicionesPrioritariasBajada={1,2,3,4}; //Corregir
+	
 	private int modoPolitica;
+	//Modo 0: aleatoria.
+    //Modo 1: primero los que suben.
+    //Modo 2: primero los que bajan.
 	
 	public Politica(int modo){
 		setModo(modo);
@@ -26,8 +32,8 @@ public class Politica {
 			boolean flag=true;
 			while(flag){
 				int numeroAleatorio = (int) (Math.random()*100);
-				indice=numeroAleatorio % listaM.size();
-				if(listaM.get(indice)!=0){
+				indice=numeroAleatorio % listaM.size(); //Defino una posicion aleatoria.
+				if(listaM.get(indice)!=0){ //Verifico si el numero aleatorio no coincide con una transicion que no se puede disparar
 					flag=false;
 				}
 			}
@@ -40,11 +46,11 @@ public class Politica {
 		
 			
 			for(int i=0;i<transicionesPrioritariasSubida.length;i++){
-		         if(listaM.get(transicionesPrioritariasSubida[i])==1){
+		         if(listaM.get(transicionesPrioritariasSubida[i])==1){ //El 1 indica que se puede disparar
 		             return transicionesPrioritariasSubida[i];
 		         }
 		     }
-		     return this.politicaAleatoria(listaM);
+		     return this.politicaAleatoria(listaM); //Si no está definida la prioridad, se utiliza la aleatoriedad.
 			
 	
 	}
@@ -53,18 +59,18 @@ public class Politica {
 		
 			
 			for(int i=0;i<transicionesPrioritariasBajada.length;i++){
-		         if(listaM.get(transicionesPrioritariasBajada[i])==1){
+		         if(listaM.get(transicionesPrioritariasBajada[i])==1){ //El 1 indica que se puede disparar
 		             return transicionesPrioritariasBajada[i];
 		         }
 		     }
-		     return this.politicaAleatoria(listaM);
+		     return this.politicaAleatoria(listaM); //Si no está definida la prioridad, se utiliza la aleatoriedad.
 			
 	
 	}
 	
 	
 	
-	
+	//Metodo que indica cual transicion se dispara segun el modo de politica elegido.
 	public int cualDisparar(List<Integer> listaM) throws IndexOutOfBoundsException{
 		if(listaM.size()>0){
 			if(this.modoPolitica==0){ //Politica aleatoria.
