d7450ca707d667ed5e23a06d339e31e4748f848c
CondorNegro <lopez.gaston.1996@gmail.com>
2018-02-07 00:29:09 -0300

    Python con invariantes

--- CodigoJava/src/RedesParaTest/html2Excel.py
+++ CodigoJava/src/RedesParaTest/html2Excel.py
@@ -3,91 +3,156 @@ import urllib
 import xlwt #Para trabajar con libros Excel
 
 #Nombres de hojas de Excel.
-arregloNombresPaginas=["Matriz I+", "Matriz I-", "Matriz I", "Matriz H", "Matriz M"] 
+#La segunda fila es para los invariantes
+arregloNombresPaginas=[["Matriz I+", "Matriz I-", "Matriz I", "Matriz H", "Matriz M"], ["T-Invariantes", "P-Invariantes"]] 
 #Titulos de tablas en HTML 
-arregloVerificacion=["Forwards incidence", "Backwards incidence","Combined incidence","Inhibition matrix", "Marking"]
+#La segunda fila es para los invariantes
+arregloVerificacion=[["Forwards incidence", "Backwards incidence","Combined incidence","Inhibition matrix", "Marking"], ["T0", "P0"]] 
 enteroVerificacion=0 #Para verificar inicio de tabla
+#Solicitud por consola
+arregloURL=["\nIngrese la URL del archivo ""Petri net incidence and marking"": ", "\nIngrese la URL del archivo ""Petri net invariant analysis results"": "]
 
-#Las tablas se repiten al recorrer con las funciones de la libreria. Este flag es para detectar la repeticion.
-flagRepeticion=False
+print 'Inicio del programa\n'
 
 libroExcel = xlwt.Workbook()
-
-url=raw_input("Ingrese la URL del archivo: ")
 nombreArchivo= raw_input("Ingrese el nombre del archivo Excel destino: ")
-try:
-	fileHTML = urllib.urlopen(url)
-except:
-	print 'No se encontro el archivo'
-	exit(1)
 
-textHTML = fileHTML.read()
-soup = BeautifulSoup(textHTML)
-#print soup.prettify()
-#print soup
-#codigo a buscar en la _URL_
-cuerpo=soup.find("body") #Localizo body del codigo fuente HTML
-tablas=cuerpo.findAll("table") #Localizo tablas del body
-contadorPaginas=0 #Contador de las paginas u hojas del Excel
-contadorTablas=0  #Contador de tablas que se van barriendo con las funciones de la libreria. (Las tablas se repiten).
-flagSwitchPagina=False #Flag para indicar el cambio de pagina u hoja de Excel
-hoja = libroExcel.add_sheet(arregloNombresPaginas[0]) #Declaro Hoja 0
-		
+flagInt=0
 
-while contadorPaginas<len(arregloNombresPaginas):
-		table=tablas[contadorTablas]
-		contadorTablas=contadorTablas+1
+while(not flagInt):
+	try:
+		flagInt=1
+		strCantTransiciones= raw_input("\nIngrese el numero de transiciones: ")
 		
-		filas = table.findAll("tr") #Las filas se indican en codigo HTML con tr.
-		x = 0
-		y= 1
+		cantTransiciones=int(strCantTransiciones)
+		if(cantTransiciones<1):
+			flagInt=0
+			print 'Ingrese un valor correcto (numero positivo natural mayor a 0).'
+	except ValueError:
+		print 'Error. Ingrese un valor correcto (numero positivo natural mayor a 0).'
+		flagInt=0
+
+flagInt=0
+
+while(not flagInt):
+	try:
+		flagInt=1
+		strCantPlazas= raw_input("\nIngrese el numero de plazas: ")
 		
-		
-		#print rows
-		
-		contadorInformacionDescartable=0
-		for tr in filas:
-			columnas = tr.findAll("td") #Las columnas se indican en codigo HTML con td.
-			if not columnas:
-				#fila vacia no se imprime nada
-				continue
-			y = 1
-			for td in columnas:
-				#print contadorPaginas
-				if(td.text.encode("utf-8").find(arregloVerificacion[contadorPaginas])!=-1): #Para detectar titulo de tabla
-					flagRepeticion=False
-					flagSwitchPagina=True
+		cantPlazas=int(strCantPlazas)
+		if(cantPlazas<1):
+			flagInt=0
+			print 'Ingrese un valor correcto (numero positivo natural mayor a 0).'
+	except ValueError:
+		print 'Error. Ingrese un valor correcto (numero positivo natural mayor a 0).'
+		flagInt=0
+
+
+for url in range(2): #Necesito 2 url. Una para Incidence and Marking. Otro para invariantes.
+
+	#Las tablas se repiten al recorrer con las funciones de la libreria. Este flag es para detectar la repeticion.
+	flagRepeticion=False
+	urlDir=raw_input(arregloURL[url])
+
+	try:
+		fileHTML = urllib.urlopen(urlDir)
+	except:
+		print 'No se encontro el archivo'
+		exit(1)
+
+	textHTML = fileHTML.read()
+	soup = BeautifulSoup(textHTML)
+	#print soup.prettify()
+	#print soup
+	#codigo a buscar en la _URL_
+	cuerpo=soup.find("body") #Localizo body del codigo fuente HTML
+	tablas=cuerpo.findAll("table") #Localizo tablas del body
+	contadorPaginas=0 #Contador de las paginas u hojas del Excel
+	contadorTablas=0  #Contador de tablas que se van barriendo con las funciones de la libreria. (Las tablas se repiten).
+	flagSwitchPagina=False #Flag para indicar el cambio de pagina u hoja de Excel
+	hoja = libroExcel.add_sheet(arregloNombresPaginas[url][0]) #Declaro Hoja 0 para cada url
+			
+
+	while contadorPaginas<len(arregloNombresPaginas[url]):
+			table=tablas[contadorTablas]
+			contadorTablas=contadorTablas+1
+			
+			filas = table.findAll("tr") #Las filas se indican en codigo HTML con tr.
+			x = 0
+			y= 1
+			
+			
+			
+			
+			contadorInformacionDescartable=0
+			#La variable contadorInformacionDescartable se utiliza para ignorar titulos y comentarios.
+			for tr in filas:
+				columnas = tr.findAll("td") #Las columnas se indican en codigo HTML con td.
+				if not columnas:
+					#fila vacia no se imprime nada
+					continue
+				y = 1
+				if(url): #Pagina de invariantes. Cambia la forma de las tablas
+					y=0
 				
-				texto = td.text.encode("utf-8").strip
-				contadorInformacionDescartable=contadorInformacionDescartable + 1
+				for td in columnas:
+					#print contadorPaginas
+					if(td.text.encode("utf-8").find(arregloVerificacion[url][contadorPaginas])!=-1): #Para detectar titulo de tabla
+						flagRepeticion=False
+						flagSwitchPagina=True
+						
+					
+					texto = td.text.encode("utf-8").strip
+					contadorInformacionDescartable=contadorInformacionDescartable + 1
 
-				
-				if(contadorInformacionDescartable>5 and not flagRepeticion):
-					if(td.text.encode("utf-8")=="" or td.text==u'T0' or (td.text=='P0' and contadorPaginas==len(arregloNombresPaginas)-1)): #Para evitar repeticion de tablas
-						flagRepeticion=True
-				
-				if(contadorInformacionDescartable>3):
-					if(not flagRepeticion):
-						#print x,y,contadorPaginas, td.text
-						if(td.text.encode("utf-8")!="" and td.text.encode("utf-8").find("P")==-1 and td.text.encode("utf-8").find("T")==-1 and td.text.encode("utf-8").find("t")==-1):
-							#Es un nro
-							hoja.write(x, y, int(td.text.encode("utf-8")))
-						else:
-							#Es un String
-							hoja.write(x, y, td.text)
-						#print(x, y, td.text)
-						y = y + 1
-						if(y>4):
-							y=0
-							x=x+1
-
-		if(flagSwitchPagina):
-			contadorPaginas=contadorPaginas+1
-			if(contadorPaginas<len(arregloNombresPaginas)):
-				hoja = libroExcel.add_sheet(arregloNombresPaginas[contadorPaginas])			
-			flagSwitchPagina=False
+					
+					if(contadorInformacionDescartable>5 and not flagRepeticion): #Control de repeticion
+						if(td.text.encode("utf-8")=="" or td.text==u'T0' or (td.text=='P0' and contadorPaginas==len(arregloNombresPaginas[url])-1)): #Para evitar repeticion de tablas
+							flagRepeticion=True 
+							#Ante todas las condiciones anteriores aparece una repeticion.
+							
+					
+					if((contadorInformacionDescartable>3 and url==0 ) or (url)): #Para los invariantes no hace falta ignorar titulos y comentarios
+						if(not flagRepeticion): #Si no hay repeticion
+							
+							if(td.text.encode("utf-8")!="" and td.text.encode("utf-8").find("P")==-1 and td.text.encode("utf-8").find("T")==-1 and td.text.encode("utf-8").find("t")==-1):
+								#Es un nro
+								hoja.write(x, y, int(td.text.encode("utf-8")))
 
 
+							else:
+								#Es un String
+								hoja.write(x, y, td.text)
+							
+							y = y + 1
+							if(url): #Para ver en que instante se comienza una nueva fila en Excel. (Ver configuracion de tablas)
+								if(not contadorPaginas):
+									if(y>cantTransiciones-1):
+										y=0
+										x=x+1
+
+								else:
+									if(y>cantPlazas-1):
+										y=0
+										x=x+1
+
+							else:
+								if(contadorPaginas<4):
+									if(y>cantTransiciones):
+										y=0
+										x=x+1
+								else:
+									if(y>cantPlazas):
+										y=0
+										x=x+1
+
+
+
+			if(flagSwitchPagina): #Cambio de pagina
+				contadorPaginas=contadorPaginas+1
+				if(contadorPaginas<len(arregloNombresPaginas[url])): #Para evitar sobrepasamiento de limites
+					hoja = libroExcel.add_sheet(arregloNombresPaginas[url][contadorPaginas])			
+				flagSwitchPagina=False
 
 
 
@@ -100,3 +165,5 @@ except:
 		print 'No se pudo guardar los datos en el archivo.'
 		exit(1)	
 		
+
+print '\nFin del programa.'
