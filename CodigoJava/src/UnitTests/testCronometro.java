f1ff844d9991fcdce34416ef3a474e73eff8ccee
CondorNegro <lopez.gaston.1996@gmail.com>
2018-02-09 00:15:19 -0300

    Estructura de unit test

---
+++ CodigoJava/src/UnitTests/testCronometro.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ */
+package UnitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class testCronometro {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link Monitor.Cronometro#Cronometro()}.
+	 */
+	@Test
+	public void testCronometro() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Monitor.Cronometro#setNuevoTimeStamp()}.
+	 */
+	@Test
+	public void testSetNuevoTimeStamp() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Monitor.Cronometro#resetearContador()}.
+	 */
+	@Test
+	public void testResetearContador() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Monitor.Cronometro#getMillis()}.
+	 */
+	@Test
+	public void testGetMillis() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Monitor.Cronometro#getSeconds()}.
+	 */
+	@Test
+	public void testGetSeconds() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Monitor.Cronometro#getContador()}.
+	 */
+	@Test
+	public void testGetContador() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ CodigoJava/src/UnitTests/testLogicaTemporal.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package UnitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class testLogicaTemporal {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link Monitor.LogicaTemporal#LogicaTemporal(int)}.
+	 */
+	@Test
+	public void testLogicaTemporal() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Monitor.LogicaTemporal#setVectorIntervalosFromExcel(jxl.Workbook)}.
+	 */
+	@Test
+	public void testSetVectorIntervalosFromExcel() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Monitor.LogicaTemporal#getVectorDeIntervalos()}.
+	 */
+	@Test
+	public void testGetVectorDeIntervalos() {
+		fail("Not yet implemented");
+	}
+
+}
