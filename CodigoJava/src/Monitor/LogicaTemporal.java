227615115fc1e3b258fdfb8f67d92945125ff632
CondorNegro <lopez.gaston.1996@gmail.com>
2018-02-08 23:34:37 -0300

    Matriz Z

--- CodigoJava/src/Monitor/LogicaTemporal.java
+++ CodigoJava/src/Monitor/LogicaTemporal.java
@@ -7,8 +7,9 @@ package Monitor;
 public class LogicaTemporal {
 	private IDVector vectorID;
 	private int[][] vectorDeIntervalos;
-	private int[][] stateVector;
-	private Cronometro timeStampsVector[];
+	private int[][] vectorDeEstado;
+	private Cronometro vectorDeTimeStamps[];
+	private int [][] vectorZ; //Contiene un uno si el contador esta entre alfa y beta. De lo contrario un cero.
 	
 	public LogicaTemporal(){
 		
