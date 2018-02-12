b19b8edc9876a871cb603ba53a6060878b642c04
ragnar <kleiner.matias@gmail.com>
2018-02-11 23:15:48 -0300

    Cambios menores en monitor

--- CodigoJava/src/Monitor/Monitor.java
+++ CodigoJava/src/Monitor/Monitor.java
@@ -163,7 +163,7 @@ public class Monitor {
 		
 		while(k){
 			k=rdp.disparar(transicion); //Disparo red de petri. //Si se logra disparar se pone en true.
-			System.out.println(k);
+			//System.out.println(k);
 			if(k){ //K=true verifica el estado de la red.
 				int[] Vs=rdp.getSensibilizadasExtendido(); //get transiciones sensibilizadas
 				int[] Vc=quienesEstanEnColas(); //get Quienes estan en colas
@@ -237,7 +237,7 @@ public class Monitor {
 		
 		while(k){
 			k=rdp.disparar(transicion); //Disparo red de petri. //Si se logra disparar se pone en true.
-			System.out.println(k);
+			System.out.println("Comenzando el thread"+ Thread.currentThread().getName()+" a usar el recursotiene el lock"); 
 			if(k){ //K=true verifica el estado de la red.
 				int[] Vs=rdp.getSensibilizadasExtendido(); //get transiciones sensibilizadas
 				int[] Vc=quienesEstanEnColas(); //get Quienes estan en colas
