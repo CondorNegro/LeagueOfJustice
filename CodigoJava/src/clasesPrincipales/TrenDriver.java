9358fc4801f9f89f2603e2e863a76c69b95d53c9
CondorNegro <lopez.gaston.1996@gmail.com>
2018-02-13 16:45:46 -0300

    Logueo en Tren Driver

--- CodigoJava/src/clasesPrincipales/TrenDriver.java
+++ CodigoJava/src/clasesPrincipales/TrenDriver.java
@@ -25,18 +25,84 @@ public class TrenDriver implements Runnable {
      */
 	public void run(){
 		HashMap<String, String> tablaLogueo=new HashMap<String, String>();
-		tablaLogueo.put("arriboA", "0");
-		tablaLogueo.put("salgoA", "1");
-		tablaLogueo.put("arriboB", "2");
-		tablaLogueo.put("salgoB", "3");
-		tablaLogueo.put("arriboC", "4");
-		tablaLogueo.put("salgoC", "5");
-		tablaLogueo.put("arriboD", "6");
-		tablaLogueo.put("salgoD", "7");
-		
+		tablaLogueo.put( "arriboA",  "0");
+		tablaLogueo.put( "salgoA",   "1");
+		tablaLogueo.put( "arriboB",  "2");
+		tablaLogueo.put( "salgoB",   "3");
+		tablaLogueo.put( "arriboC",  "4");
+		tablaLogueo.put( "salgoC",   "5");
+		tablaLogueo.put( "arriboD",  "6");
+		tablaLogueo.put( "salgoD",   "7");
+		boolean flagImpresion=true;
 		
 		while(true){
+			if(flagImpresion){
+				System.out.println("Tren en A");
+				
+			}
+			this.log.addMessage(tablaLogueo.get("arriboA"), this.indiceLog);
 			
+			//Ver transiciones a disparar
+			
+			if(flagImpresion){
+				System.out.println("Tren saliendo de A");
+				
+			}
+			this.log.addMessage(tablaLogueo.get("salgoA"), this.indiceLog);
+			
+			//Ver transiciones a disparar
+			
+			if(flagImpresion){
+				System.out.println("Tren en B");
+				
+			}
+			this.log.addMessage(tablaLogueo.get("arriboB"), this.indiceLog);
+			
+			//Ver transiciones a disparar
+			
+			if(flagImpresion){
+				System.out.println("Tren saliendo de B");
+				
+			}
+			this.log.addMessage(tablaLogueo.get("salgoB"), this.indiceLog);
+			
+			//Ver transiciones a disparar
+			
+			if(flagImpresion){
+				System.out.println("Tren en C");
+				
+			}
+			this.log.addMessage(tablaLogueo.get("arriboC"), this.indiceLog);
+			
+			//Ver transiciones a disparar
+			
+			if(flagImpresion){
+				System.out.println("Tren saliendo de C");
+				
+			}
+			this.log.addMessage(tablaLogueo.get("salgoC"), this.indiceLog);
+			
+			
+			//Ver transiciones a disparar
+			
+			if(flagImpresion){
+				System.out.println("Tren en D");
+				
+			}
+			this.log.addMessage(tablaLogueo.get("arriboD"), this.indiceLog);
+			
+			
+			//Ver transiciones a disparar
+			
+			if(flagImpresion){
+				System.out.println("Tren saliendo de D");
+				
+			}
+			this.log.addMessage(tablaLogueo.get("salgoD"), this.indiceLog);
+			
+			//Ver transiciones a disparar
+			
+			this.monitor.dispararTransicion(1); //Entrada a la estacion A
 		}
 	}
 	
